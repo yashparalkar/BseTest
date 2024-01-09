0681a247c35b7c5080efb5d876833452806947d5
Yash Paralkar <97403504+yashparalkar@users.noreply.github.com>
2024-01-09 13:00:11 +0530

    main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ pb = PushBullet(access_token)
 
 bse = BSE(update_codes=True)
 
-stock = []
+stocks = []
 codes = [
     543272, 532368, 532648, 532670, 539436, 543331, 532667, 500285, 532822,
     532666, 542655, 543688, 535113, 542655
@@ -24,52 +24,7 @@ stock_history = [{code: None
                   for code in codes}]
 
 # Define the trading hours
-start_time = time(3, 30)
-end_time = time(10, 0)
-
-while True:
-    for _i, code in enumerate(codes):
-        try:
-          quote = bse.getQuote(str(code))
-          quote.pop('buy')
-          quote.pop('sell')
-
-          # Update stock history for the current interval
-          stock_history[2][code] = stock_history[1][code]
-          stock_history[1][code] = stock_history[0][code]
-          stock_history[0][code] = quote
-
-          if all(stock_history[2].values()) and all(
-              stock_history[1].values()) and all(stock_history[0].values()):
-            # Calculate the percentage change between 20 minutes ago and now
-            percent_change_30min_ago = (
-                (float(stock_history[0][code]['currentValue']) -
-                 float(stock_history[2][code]['currentValue'])) /
-                float(stock_history[2][code]['currentValue'])) * 100
-
-            # Calculate the percentage change between 10 minutes ago and now
-            percent_change_15min_ago = (
-                (float(stock_history[0][code]['currentValue']) -
-                 float(stock_history[1][code]['currentValue'])) /
-                float(stock_history[1][code]['currentValue'])) * 100
-
-            if abs(percent_change_30min_ago) >= 0 or abs(
-                percent_change_15min_ago) >= 0:
-              # Send an email notification
-              subject = f"Stock {stock_history[0][code]['companyName']} Swing Alert"
-              body = f"Stock {stock_history[0][code]['companyName']} has changed "
-              body += f"by {percent_change_15min_ago:.2f}% in the last 15 minutes\n\n" if abs(
-                  percent_change_15min_ago
-              ) >= 1 else f"by {percent_change_30min_ago:.2f}% in the last 30 minutes.\n\n"
-              body += f"30min Ago Value: {stock_history[2][code]['currentValue']}\n"
-              body += f"15min Ago Value: {stock_history[1][code]['currentValue']}\n"
-              body += f"Current Value: {stock_history[0][code]['currentValue']}\n"
-              message = f"Subject: {subject}\n\n{body}"
-
-              push = pb.push_note(subject, body)
-
-        except bsedata.exceptions.InvalidStockException as e:
-          print(f"Ignoring inactive stock with code {code}: {e}")
-
-  # Sleep for a minute before checking again
-    t.sleep(60)
+for code in codes:
+  stocks.append(getQuote(str(code)))
+  for stock in stocks:
+    pb.push_note("Stock Price", f"Price of {stock['companyName']} is {stock['currentValue']}")
