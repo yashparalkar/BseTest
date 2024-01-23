c679f9882bd24a1bab1af6e965851346e183d376
Yash Paralkar <97403504+yashparalkar@users.noreply.github.com>
2024-01-23 13:08:56 +0530

    main.py

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ while True:
                              float(stock_history[1][code]['currentValue'])) /
                             float(stock_history[1][code]['currentValue'])) * 100
                     
-                        if abs(percent_change_30min_ago) >= 0 or abs(
-                            percent_change_15min_ago) >= 0:
+                        if abs(percent_change_30min_ago) >= 1 or abs(
+                            percent_change_15min_ago) >= 1:
                           # Send an email notification
                               subject = f"Stock {stock_history[0][code]['companyName']} Swing Alert"
                               body = f"Stock {stock_history[0][code]['companyName']} has changed "
@@ -70,6 +70,21 @@ while True:
                               except RequestException as e:
                                   logging.error(f"Request Exception: {e}", exc_info=True)
                               conn = None
+                              try:
+                                  conn = http.client.HTTPSConnection("api.pushover.net:443")
+                                  conn.request(
+                                      "POST", "/1/messages.json",
+                                      urllib.parse.urlencode({
+                                        "token": "ae7qnfbucxiojph9pip8ht4snfj2ji",
+                                        "user": "uuxzqfp8x77jkib2ymb8a65ub1688h",
+                                        "message": f"{subject}\n{body}",
+                                    }), {"Content-type": "application/x-www-form-urlencoded"})
+                                  conn.getresponse()
+                              except Exception as e:
+                                  print(f"ConnectionError: {e}")
+                              finally:
+                                  if conn:
+                                      conn.close()
                             
                 except bsedata.exceptions.InvalidStockException as e:
                     print(f"Ignoring inactive stock with code {code}: {e}")
