8316d1340da440c5d54d14f1098ea0e736d2e67c
Yash Paralkar <97403504+yashparalkar@users.noreply.github.com>
2024-04-07 21:25:29 +0530

    main.py

--- main.py
+++ main.py
@@ -65,15 +65,15 @@ while True:
                             message = f"Subject: {subject}\n\n{body}"
                         
                             try:
-                                conn = http.client.HTTPSConnection("api.pushover.net:443")
-                                conn.request(
-                                     "POST", "/1/messages.json",
-                                     urllib.parse.urlencode({
-                                     "token": os.environ.get('token'),
-                                     "user": os.environ.get("user"),
-                                     "message": f"{subject}\n{body}",
-                                }), {"Content-type": "application/x-www-form-urlencoded"})
-                                conn.getresponse()
+                                # conn = http.client.HTTPSConnection("api.pushover.net:443")
+                                # conn.request(
+                                #      "POST", "/1/messages.json",
+                                #      urllib.parse.urlencode({
+                                #      "token": os.environ.get('token'),
+                                #      "user": os.environ.get("user"),
+                                #      "message": f"{subject}\n{body}",
+                                # }), {"Content-type": "application/x-www-form-urlencoded"})
+                                # conn.getresponse()
                                 conn1 = http.client.HTTPSConnection("api.pushover.net:443")
                                 conn1.request(
                                     "POST", "/1/messages.json",
@@ -87,7 +87,7 @@ while True:
                                 print(f"ConnectionError: {e}")
                             finally:
                                 if conn1:
-                                    conn.close()
+                                    # conn.close()
                                     conn1.close()
                             
                 except bsedata.exceptions.InvalidStockException as e:
