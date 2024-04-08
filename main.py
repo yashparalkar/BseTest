c6a5d4af84af99d61a6098c2bf0fb36c9ae3165e
Yash Paralkar <97403504+yashparalkar@users.noreply.github.com>
2024-04-08 13:08:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ bse = BSE(update_codes=True)
 start_time = time(3, 0)
 end_time = time(10, 30)
 
-# codes = [ 543272, 532368, 532648, 532670, 539436, 543331, 532667, 500285, 532822,
-#      542655, 543688, 500116]
-codes = [543272, 532368]
+codes = [ 543272, 532368, 532648, 532670, 539436, 543331, 532667, 500285, 532822,
+     542655, 543688, 500116]
+# codes = [543272, 532368]
 stock_history = [{code: None for code in codes}, {code: None for code in codes}, {code: None for code in codes}]
 
 # Define the trading hours
@@ -51,8 +51,8 @@ while True:
                              float(stock_history[1][code]['currentValue'])) /
                             float(stock_history[1][code]['currentValue'])) * 100
                     
-                        if abs(percent_change_30min_ago) >= 0 or abs(
-                            percent_change_15min_ago) >= 0:
+                        if abs(percent_change_30min_ago) >= 1.5 or abs(
+                            percent_change_15min_ago) >= 1.5:
                           # Send an email notification
                             subject = f"Stock {stock_history[0][code]['companyName']} Swing Alert"
                             body = f"Stock {stock_history[0][code]['companyName']} has changed "
@@ -80,7 +80,7 @@ while True:
                                     urllib.parse.urlencode({
                                     "token": os.environ.get('my_token'),
                                     "user": os.environ.get("my_user_id"),
-                                    "message": f"{subject}\n{body}\n{now}",
+                                    "message": f"{subject}\n{body}",
                                 }), {"Content-type": "application/x-www-form-urlencoded"})
                                 conn1.getresponse()
                             except RequestException as e:
@@ -94,4 +94,4 @@ while True:
                     print(f"Ignoring inactive stock with code {code}: {e}")
 
   # Sleep for a minute before checking again
-    t.sleep(60)
+    t.sleep(900)
